64eaebf62ebea21cde494f84979765102e016124
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-11-05 20:29:56 +0300

    add modal for calling and validation for tel

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
 const body = document.getElementsByTagName('body')[0];
+const modalCallingBtn = document.getElementsByClassName('modal-for-calling')[0]
+
 
 body.onclick = (e) => {
     const target = e.target;
     const modalElem = target.parentElement.parentElement.children[1];
 
+    if (target.classList.contains('modal-calling')) {
+        return openModal(modalCallingBtn)
+    }
+
     if (target.classList.contains('types-coverings__item-btn')) {
-        openModal(modalElem)
+        return openModal(modalElem)
+
     } else if (target.classList.contains('show')) {
-        closeModal(target)
+        return closeModal(target)
+
     } else if (target.classList.contains('modal__close')) {
         const modal = target.parentElement.parentElement;
-        closeModal(modal)
+        return closeModal(modal)
     }
 
     if (target.classList.contains('trigger')) {
-        const modalWork = target.parentElement.parentElement.parentElement.children[1];
-        openModal(modalWork)
-    }
 
+        const modalWork = target.parentElement.parentElement.parentElement.children[1];
+        return openModal(modalWork)
+    }
 }
 
 
@@ -33,14 +41,3 @@ function openModal(el) {
     document.body.style.overflow = 'hidden';
 }
 
-function foo(arr) {
-    let str = '';
-    let sql = `delete from nodes where id in`
-
-    arr.forEach(item => {
-        str += `${item},`
-    })
-    sql +=` (${str.slice(0,-1)})`
-    console.log(sql)
-}
-foo([2,3,4,5,6,7])
