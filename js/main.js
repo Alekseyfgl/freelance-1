f180f5a0106675137cdadecd4519941b7e7bc3cc
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-11-05 20:53:02 +0300

    add anchors

--- js/main.js
+++ js/main.js
@@ -41,3 +41,17 @@ function openModal(el) {
     document.body.style.overflow = 'hidden';
 }
 
+
+
+const anchors = document.querySelectorAll('a[href^="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', (event) => {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
