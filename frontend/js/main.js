a97bd8008a311a8a3397a9df12dc90c9ea1037de
aleksey <Aleksey.fgl@gmail.com>
2022-12-19 23:22:17 +0300

    add sending massage

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,75 +1,68 @@
-const body = document.getElementsByTagName('body')[0];
-const modalCallingBtn = document.getElementsByClassName('modal-for-calling')[0]
-const header = document.getElementsByClassName('header')[0];
+const body = document.getElementsByTagName("body")[0];
+const modalCallingBtn = document.getElementsByClassName("modal-for-calling")[0];
+const header = document.getElementsByClassName("header")[0];
 
-header.addEventListener('click', (event) => {
-    const target = event.target;
-    // const body = document.getElementsByTagName('body')[0];
-    const btnBurger = document.getElementsByClassName('header-burger')[0];
-    console.log(btnBurger)
+header.addEventListener("click", (event) => {
+  const target = event.target;
+  const btnBurger = document.getElementsByClassName("header-burger")[0];
+  console.log(btnBurger);
 
-    if (target.classList.contains('header-burger')) {
-        btnBurger.classList.toggle('active');
-        header.classList.toggle('active');
-        body.classList.toggle('block')
-
-    } else if (target.tagName === 'LI' || target.tagName === 'A') {
-        btnBurger.classList.remove('active');
-        header.classList.remove('active');
-        body.classList.remove('block')
-    }
+  if (target.classList.contains("header-burger")) {
+    btnBurger.classList.toggle("active");
+    header.classList.toggle("active");
+    body.classList.toggle("block");
+  } else if (target.tagName === "LI" || target.tagName === "A") {
+    btnBurger.classList.remove("active");
+    header.classList.remove("active");
+    body.classList.remove("block");
+  }
 });
 
 body.onclick = (e) => {
-    const target = e.target;
-    const modalElem = target.parentElement.parentElement.children[1];
+  const target = e.target;
+  const modalElem = target.parentElement.parentElement.children[1];
 
-    if (target.classList.contains('modal-calling')) {
-        return openModal(modalCallingBtn)
-    }
+  if (target.classList.contains("modal-calling")) {
+    return openModal(modalCallingBtn);
+  }
 
-    if (target.classList.contains('types-coverings__item-btn')) {
-        return openModal(modalElem)
+  if (target.classList.contains("types-coverings__item-btn")) {
+    return openModal(modalElem);
+  } else if (target.classList.contains("show")) {
+    return closeModal(target);
+  } else if (target.classList.contains("modal__close")) {
+    const modal = target.parentElement.parentElement;
+    return closeModal(modal);
+  }
 
-    } else if (target.classList.contains('show')) {
-        return closeModal(target)
-
-    } else if (target.classList.contains('modal__close')) {
-        const modal = target.parentElement.parentElement;
-        return closeModal(modal)
-    }
-
-    if (target.classList.contains('trigger')) {
-
-        const modalWork = target.parentElement.parentElement.parentElement.children[1];
-        return openModal(modalWork)
-    }
-}
-
+  if (target.classList.contains("trigger")) {
+    const modalWork =
+      target.parentElement.parentElement.parentElement.children[1];
+    return openModal(modalWork);
+  }
+};
 
 function closeModal(el) {
-    el.classList.remove('show')
-    el.classList.add('hide');
-    document.body.style.overflow = '';
+  el.classList.remove("show");
+  el.classList.add("hide");
+  document.body.style.overflow = "";
 }
 
 function openModal(el) {
-    el.classList.remove('hide')
-    el.classList.add('show');
-    document.body.style.overflow = 'hidden';
+  el.classList.remove("hide");
+  el.classList.add("show");
+  document.body.style.overflow = "hidden";
 }
-
-
 
 const anchors = document.querySelectorAll('a[href^="#"]');
 
 for (let anchor of anchors) {
-    anchor.addEventListener('click', (event) => {
-        event.preventDefault();
-        const blockID = anchor.getAttribute('href')
-        document.querySelector('' + blockID).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-    })
+  anchor.addEventListener("click", (event) => {
+    event.preventDefault();
+    const blockID = anchor.getAttribute("href");
+    document.querySelector("" + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
 }
